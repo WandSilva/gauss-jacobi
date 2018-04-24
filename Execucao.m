193a59c50c5ae0c02e23a5827ff27c60db6ae393
IcaroRios <rockicarorios@gmail.com>
2018-04-23 23:50:34 -0300

    fim do algoritmo

---
+++ Execucao.m
@@ -0,0 +1,7 @@
+F = [ %matriz aumentada
+    3,-1,2,3;
+    1,1,0,2;
+    -2,3,-4,3];
+clc
+fprintf('Iniciando a Execução \n')
+jacobi(F)
--- jacobi.m
+++ jacobi.m
@@ -1,40 +1,45 @@
-A = [ %matriz de equacoes
-    3, -1,2;
-    1,1,0;
-    -2,3,-4];
-
-b = [3; %matriz resposta 
-     2;
-     3];
-
-m = 100; %iteracoes
-E = 0.000001; %erro
-
-n = length(b);
-
-XO = zeros(1,n);
-X = XO; %resposta
-k=0;
-
-norma = 1;
-
-while norma>E
-  k= k+1;
-  fprintf('\niteração %d\n',k);
-  for i = 1 : n  % loop of equations
-    Sum = 0;
-    for j = 1 : n %loop of summation
-      if j ~= i
-        Sum = Sum + A(i,j)* X(j);
-      end
+function [] = Untitled2( matrizAumentada)
+    [m,n] = size(matrizAumentada)
+    A = zeros(n-1)
+    b = zeros(m,1)
+    for j=1:m
+        for i =1:n
+            if i~=n
+                A(j,i) = matrizAumentada(j,i)
+            else
+                b(j,1) = matrizAumentada(j,i)
+            end
+        end
     end
-    X(i) = (b(i)-Sum)/A(i,i);
-    fprintf('incognita %d',i);  
-    fprintf('%10.6f\n',X(i));    
-  end
-  norma = norm(XO-X);
-  XO = X;
-  if k>=m
-    break
-  end
+
+    m = 1000; %iteracoes
+    E = 0.000001; %erro
+
+    n = length(b);
+    XO = zeros(1,n);
+    X = XO; %resposta
+    k=0;
+
+    norma = 1;
+    fprintf('Iniciando Método Iterativo')
+    while norma>E
+      k= k+1;
+      fprintf('\niteração %d\n',k);
+     for i = 1 : n  % loop of equations
+       Sum = 0;
+       for j = 1 : n %loop of summation
+         if j ~= i
+           Sum = Sum + A(i,j)* X(j);
+         end
+       end
+       X(i) = (b(i)-Sum)/A(i,i);
+       fprintf('incognita %d',i);  
+       fprintf('%10.6f\n',X(i));    
+     end
+      norma = norm(XO-X);
+      XO = X;
+     if k>=m
+       break
+     end
+    end
 end
