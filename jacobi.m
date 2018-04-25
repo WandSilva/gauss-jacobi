16347a6d5b8f29c14618faf3ecdb3d5ccf7780e3
Wanderson Silva <bsilva.wanderson@gmail.com>
2018-04-24 23:46:44 -0300

    colocando a saída em formato de tabela

--- jacobi.m
+++ jacobi.m
@@ -21,25 +21,32 @@ function [] = jacobi( matrizAumentada)
     k=0;
 
     norma = 1;
-    fprintf('Iniciando Método Iterativo')
-    while norma>E
-      k= k+1;
-      fprintf('\niteração %d\n',k);
-     for i = 1 : n
-       Sum = 0;
-       for j = 1 : n
-         if j ~= i
-           Sum = Sum + A(i,j)* X(j);
-         end
-       end
-       X(i) = (b(i)-Sum)/A(i,i);
-       fprintf('incognita %d',i);  
-       fprintf('%10.6f\n',X(i));    
-     end
-      norma = norm(XO-X);
-      XO = X;
-     if k>=m
-       break
-     end
+
+    fprintf('Iniciando MÃ©todo Iterativo\n')
+    fprintf('iteraÃ§Ã£o   |');
+    for t = 1:n
+        fprintf('  incognita %d  ',t);
     end
-end
+
+    while norma>E
+        k= k+1;
+        fprintf('\n      %d    |',k);
+        for i = 1 : n
+            Sum = 0;
+            for j = 1 : n
+                if j ~= i
+                    Sum = Sum + A(i,j)* X(j);
+                end
+            end
+            X(i) = (b(i)-Sum)/A(i,i);
+
+            fprintf(' %10.6f   |',X(i));
+        end
+        norma = norm(XO-X);
+        XO = X;
+        if k>=m
+            break
+        end
+
+    end
+    end
